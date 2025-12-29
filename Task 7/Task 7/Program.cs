fe13da7c44c27134597733cebbdd0cfe69157154
olafl <olafl@LAPTOP>
2025-12-29 01:29:06 +0100

    task description

--- Task 7/Task 7/Program.cs
+++ Task 7/Task 7/Program.cs
@@ -4,6 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ 
+Design a function named Rand which implements the MRG32k3a algorithm. The function should return a real value from the   interval. Then, use the Rand function to design and implement the following functions:
+
+RandInt(a, b) which returns a random integer between a and b,
+RandFloat(a, b) which returns a random real number between a and b,
+RandElement(A) which returns a random element from the array of real
+numbers A,
+RandString(a) which returns a random string of lower- and upper-case letters, digits, and special symbols, of the length a.
+ 
+ */
+
 namespace Task_7 {
     internal class Program {
         static void Main(string[] args) {
