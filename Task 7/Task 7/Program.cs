cafbd9a9c7907080579bb80627a67e71bddcd61b
olafl <olafl@LAPTOP>
2026-01-03 19:38:03 +0100

    still stack overflow

--- Task 7/Task 7/Program.cs
+++ Task 7/Task 7/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 /*
@@ -26,8 +27,12 @@ namespace Task_7 {
         // constant terms for m1 and m2
         private static readonly double m1 = Math.Pow(2, 32) - 209;
         private static readonly double m2 = Math.Pow(2, 32) - 22853.0;
+        // storage for previous results
         static Dictionary<long,double> memoizationA = new Dictionary<long, double>();
         static Dictionary<long, double> memoizationB = new Dictionary<long, double>();
+
+        //
+        // public Thread(System.Threading.ThreadStart start, int maxStackSize);
 
         static void Main(string[] args) {
             Console.WriteLine(Rand());
@@ -47,6 +52,7 @@ namespace Task_7 {
             if (i <= 0) return 1; // base case
             // save results to reduce calculation load on larger i values
             if (memoizationA.ContainsKey(i)) return memoizationA[i];
+
             Console.WriteLine($"A{i}");
             memoizationA.Add(i, ((1403580.0 * A(i - 2)) - (810728.0 * A(i - 3))) % m1);
             return memoizationA[i];
@@ -57,6 +63,7 @@ namespace Task_7 {
             if (i <= 0) return 1; // base case
             // save results to reduce calculation load on larger i values
             if (memoizationB.ContainsKey(i)) return memoizationB[i];
+
             Console.WriteLine($"B{i}");
             memoizationB.Add(i, ((527612.0 * B(i - 1)) - (1370589.0 * B(i - 3))) % m2);
             return memoizationB[i];
@@ -64,7 +71,11 @@ namespace Task_7 {
 
         static double Rand() {
             // TODO recursive => itterative
-            long time = long.Parse(DateTime.Now.ToFileTimeUtc().ToString().Substring(7));
+            // long time = long.Parse(DateTime.Now.ToFileTimeUtc().ToString().Substring(7));
+            long time = long.Parse(DateTime.Now.ToFileTimeUtc().ToString().Substring(14));
+            // long time = long.Parse(DateTime.Now.ToFileTimeUtc().ToString().Substring(0));
+            Console.WriteLine($"time: {time} | {time.ToString().Length}");
+            Console.ReadKey();
             return MRG32k3a(time) / m1;
         }
 
